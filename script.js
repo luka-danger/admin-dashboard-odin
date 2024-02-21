c51b3c98ceb19d00c2d016cdcb6b2551800ec5b2
luka-danger <lukathevegan@gmail.com>
2024-02-21 01:16:42 -0700

    Almost done with dark mode

--- script.js
+++ script.js
@@ -5,13 +5,21 @@ function darkMode(e) {
     let darkIcons = document.querySelectorAll('.header-icon');
     darkIcons.forEach(darkIcons => {
         darkIcons.classList.toggle('dark-mode');
-    })
+    });
 
     let darkSearchBar = document.querySelector('.search-bar');
     darkSearchBar.classList.toggle('dark-mode');
 
+    let darkAvatar = document.querySelector('.avatar');
+    darkAvatar.classList.toggle('dark-mode');
+
+    let darkIcon = document.querySelector('.dark-icon');
+    darkIcon.classList.toggle('dark-mode');
+
     let darkButton = document.querySelectorAll('.header-button');
-    darkButton.classList.toggle('dark-mode');
+    darkButton.forEach(darkButton => {
+        darkButton.classList.toggle('dark-mode');
+    });
 
     let profilePics = document.querySelectorAll('.profile-pic');
     profilePics.forEach(pic => pic.classList.toggle('#profile-pic-dark'));
